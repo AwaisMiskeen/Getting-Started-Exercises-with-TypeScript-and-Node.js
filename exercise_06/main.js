b175df262f1c7dbb39e7b8bdfce972e50e6032bc
awaismiskeen <awaismiskeen2005@gmail.com>
2024-02-22 00:01:24 +0500

    I complete practice exercise_06.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_06/main.js
@@ -0,0 +1,8 @@
+var white_Space_Name = "\t Asad \t";
+console.log(white_Space_Name);
+var line_bfr_after_Name = "\n Asad \n";
+console.log(line_bfr_after_Name);
+var white_Space_Line_Name = "\n\t Asad \n\t";
+console.log(white_Space_Line_Name);
+var No_space_Line_Name = white_Space_Line_Name.trim();
+console.log(No_space_Line_Name);
---
+++ exercise_06/main.ts
@@ -0,0 +1,11 @@
+let white_Space_Name = "\t Asad \t";
+console.log(white_Space_Name);
+
+let line_bfr_after_Name = "\n Asad \n";
+console.log(line_bfr_after_Name);
+
+let white_Space_Line_Name = "\n\t Asad \n\t";
+console.log(white_Space_Line_Name);
+
+let No_space_Line_Name = white_Space_Line_Name. trim();
+console.log(No_space_Line_Name);
