ca392dfa450f0799d8a0208aaf0ad444ff8f0582
awaismiskeen <awaismiskeen2005@gmail.com>
2024-02-22 09:24:49 +0500

    I complete practice exercise_11.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_11/main.js
@@ -0,0 +1,4 @@
+//use aray Function
+var Friends_Name = ["Asad", "Tariq", "Daniyal", "Awais"];
+//use foreach
+Friends_Name.forEach(function (friend_Name) { return console.log(friend_Name); });
---
+++ exercise_11/main.ts
@@ -0,0 +1,5 @@
+//use aray Function
+let Friends_Name = ["Asad","Tariq","Daniyal","Awais"];
+
+//use foreach function. Name show one by one
+Friends_Name.forEach(friend_Name => console.log(friend_Name));
