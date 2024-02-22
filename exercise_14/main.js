e2e4da84fe900b1e16ede551533b10b7ca2ad134
awaismiskeen <awaismiskeen2005@gmail.com>
2024-02-22 23:04:42 +0500

    I complete practice exercise_14.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_14/main.js
@@ -0,0 +1,2 @@
+var invite = ["Asad", "Daniyal", "Tariq"];
+invite.forEach(function (guest) { return console.log("Hello ".concat(guest, ", would you like to dinner with me?")); });
---
+++ exercise_14/main.ts
@@ -0,0 +1,3 @@
+let invite = ["Asad","Daniyal","Tariq"];
+
+invite.forEach(guest => console.log(`Hello ${guest}, would you like to dinner with me?`));
