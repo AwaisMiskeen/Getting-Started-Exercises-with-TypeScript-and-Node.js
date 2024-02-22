d8e17e7549743aead0a7987ce1fb99ddcccfe421
awaismiskeen <awaismiskeen2005@gmail.com>
2024-02-22 08:53:37 +0500

    I complete practice exercise_09.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_09/main.js
@@ -0,0 +1,3 @@
+var Favorite_Number = 2;
+var message = "My favorit number is:";
+console.log(message, Favorite_Number);
---
+++ exercise_09/main.ts
@@ -0,0 +1,4 @@
+let Favorite_Number = 2;
+let message ="My favorit number is:";
+
+console.log(message, Favorite_Number);
