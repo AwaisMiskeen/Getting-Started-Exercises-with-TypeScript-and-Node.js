ee3705d40f41df837d2661ba22d1df6349d96ce4
awaismiskeen <awaismiskeen2005@gmail.com>
2024-02-22 22:43:23 +0500

    I complete practice exercise_12.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_12/main.js
@@ -0,0 +1,2 @@
+var Friends_Name = ["Asad", "Daniyal", "Tariq", "Awais"];
+Friends_Name.forEach(function (Friend_Name) { return console.log("Hello ".concat(Friend_Name, ", How are You ?")); });
---
+++ exercise_12/main.ts
@@ -0,0 +1,3 @@
+let Friends_Name = ["Asad","Daniyal","Tariq","Awais"];
+
+Friends_Name.forEach(Friend_Name => console.log(`Hello ${Friend_Name}, How are You ?`))
