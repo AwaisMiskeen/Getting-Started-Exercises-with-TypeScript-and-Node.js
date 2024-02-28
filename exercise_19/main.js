f8af56bf9e4674422c879d8fe6c66a886484671f
awaismiskeen <awaismiskeen2005@gmail.com>
2024-02-28 15:45:31 +0500

    I complete practice exercise_19.

---
+++ exercise_19/main.js
@@ -0,0 +1,4 @@
+var invite = ["Asad", "Daniyal", "Tariq"];
+//invite.forEach(guest => console.log(`Hello ${guest}, would you like to dinner with me?`));
+var total_Invite_guests = invite.length;
+console.log("We are inviting total ".concat(total_Invite_guests, " guests."));
---
+++ exercise_19/main.ts
@@ -0,0 +1,7 @@
+let invite = ["Asad","Daniyal","Tariq"];
+
+//invite.forEach(guest => console.log(`Hello ${guest}, would you like to dinner with me?`));
+
+let total_Invite_guests: number=invite.length;
+
+console.log(`We are inviting total ${total_Invite_guests} guests.`);
