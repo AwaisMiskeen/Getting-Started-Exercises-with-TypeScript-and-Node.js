94b0b61f897904068e5915639d86b9e08f640b27
awaismiskeen <awaismiskeen2005@gmail.com>
2024-02-28 16:03:40 +0500

    I complete practice exercise_20.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_20/main.js
@@ -0,0 +1,5 @@
+var programming_languages = ["HTML", "CSS", "Javascript", "Typescript", "Python"];
+console.log("List of Programming_Languages");
+programming_languages.forEach(function (list) { return console.log(list); });
+var total = programming_languages.length;
+console.log(total);
---
+++ exercise_20/main.ts
@@ -0,0 +1,8 @@
+//making a programming languages array
+let programming_languages: string[] = ["HTML","CSS","Javascript","Typescript","Python"];
+
+//print the message of list
+console.log("List of Programming_Languages");
+
+//prin the value of list in the form of list
+programming_languages.forEach(list => console.log(list));
