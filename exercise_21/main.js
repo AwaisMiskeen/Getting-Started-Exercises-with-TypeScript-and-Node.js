f45193ea9b3fb31ec239ea54efba8cf4266e1571
awaismiskeen <awaismiskeen2005@gmail.com>
2024-03-01 08:25:07 +0500

    I complete practice exercise_21.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_21/main.js
@@ -0,0 +1,7 @@
+var IT_Course = {
+    Course_Name: "Certified Cloud Applied Generative AI Engineer",
+    Location: "Governor House Sindh",
+    On_Site_Students: 50000
+};
+//print object
+console.log(IT_Course);
---
+++ exercise_21/main.ts
@@ -0,0 +1,14 @@
+//Creating the object 
+interface IT_Course {
+    Course_Name: string;
+    Location: string;
+    On_Site_Students:number;
+}
+let IT_Course = {
+    Course_Name : "Certified Cloud Applied Generative AI Engineer",
+    Location : "Governor House Sindh",
+    On_Site_Students : 50000
+};
+
+//print object
+console.log(IT_Course)
