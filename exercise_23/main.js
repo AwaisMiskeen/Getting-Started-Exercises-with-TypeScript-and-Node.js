13fbcc40772c4824b3a11e7b2d5d6c9c40335cab
awaismiskeen <awaismiskeen2005@gmail.com>
2024-03-01 09:19:08 +0500

    I complete practice exercise_23.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_23/main.js
@@ -0,0 +1,33 @@
+//making a variable
+var two = 2;
+var six = 6;
+//Test 1
+console.log("Test 1:Two is equal to 2?");
+console.log(two == 2);
+//Test 2
+console.log("\nTest 2:Six is equal to 6?");
+console.log(six == 6);
+//Test 3
+console.log("\nTest 3:Six is not equal to 2?");
+console.log(six != 2);
+//Test 4
+console.log("\nTest 4:Six is greater than 2?");
+console.log(six > 2);
+//Test 5
+console.log("\nTest 5:Two is smaler than 6?");
+console.log(two < 6);
+//Test 6
+console.log("\nTest 6:Two is equal to 6?");
+console.log(two == 6);
+//Test 7
+console.log("\nTest 7:Six is equal to 2?");
+console.log(six == 2);
+//Test 8
+console.log("\nTest 8:Six is not equal to 6?");
+console.log(six != 6);
+//Test 9
+console.log("\nTest 9:Two is greater than 6?");
+console.log(two > 6);
+//Test 10
+console.log("\nTest 10:Six is smaler than 2?");
+console.log(six < 2);
