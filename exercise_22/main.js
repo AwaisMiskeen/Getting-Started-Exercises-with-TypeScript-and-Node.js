48ed4ec6726bd5abb7610c26d6bd957a10fc405d
awaismiskeen <awaismiskeen2005@gmail.com>
2024-03-01 08:36:44 +0500

    I complete practice exercise_22.
    Signed-off-by: awaismiskeen <awaismiskeen2005@gmail.com>

---
+++ exercise_22/main.js
@@ -0,0 +1,5 @@
+//making aray
+var error_aray = ["Awais", "Asad", "Daniyal", "Tariq"];
+//producing error by accesssing invalid index of aray
+//console.log(error_aray[6]);
+console.log(error_aray[2]);
---
+++ exercise_22/main.ts
@@ -0,0 +1,8 @@
+//making aray
+let error_aray :string [] =["Awais","Asad","Daniyal","Tariq"];
+
+//producing error by accesssing invalid index of aray
+//console.log(error_aray[6]);
+
+console.log(error_aray[2])
+
